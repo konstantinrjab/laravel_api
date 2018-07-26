b300adc0ec05d9c514b47f74e964949669a7f6f3
kryabchenko <kryabchenko@varteq.com>
2018-07-26 14:19:31 +0300

    view

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -7,12 +7,16 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                Categories
+                <h2>Categories</h2>
             </div>
         </div>
         <div class="row">
             @foreach($categories as $category)
-                <div class="">{{$category}}</div>
+                <div class="card col-6 col-sm-4 col-md-3 col-lg-2">
+                    <p>
+                        <a href="categories/{{$category->id}}">{{$category->name}}</a>
+                    </p>
+                </div>
             @endforeach
         </div>
     </div>
